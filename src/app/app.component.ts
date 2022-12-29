83fc0802d02b01a0c0bb8c0132f04c293a0a85ae
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2022-12-29 19:21:00 -0300

    Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dados';
+
+  dadoIzquierda = "../assets/images/dice1.png"
+  dadoDerecha = "../assets/images/dice5.png"
+  numero1: number = 0;
+  numero2: number = 1;
+
+  tirarDados(): void{
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierda = "../assets/images/dice" + this.numero1 +".png"
+    this.dadoDerecha = "../assets/images/dice" + this.numero2 + ".png"
+
+  }
 }
